8e085835ae4dacc38b6ed2bad195137c954f878e
Stella Kinja <kinjastella@gmail.com>
2024-06-01 06:38:36 +0300

    TDD progress

--- Documents/Moringa/week4/test.js
+++ Documents/Moringa/week4/test.js
@@ -5,4 +5,20 @@ const myFunction = require('./index');
 test('it can perform basic arithmetic', () => {
     //assertion method
     expect (myFunction(1,2)).toBe(3); 
+
+})
+test('it can detect failed arithmetic', () => {
+    //assertion method
+    expect (myFunction(1,2)).not.toBe(4); 
+
+})
+test('it can perform basic arithmetic with numbers', () => {
+    //assertion method
+    expect (myFunction(1,2)).toBe(3); 
+
+})
+test('it detect parameters that are not numbers', () => {
+    //assertion method
+    expect(() => myFunction('hello','world')).toThrow('Please provide valid numbers'); 
+
 })
