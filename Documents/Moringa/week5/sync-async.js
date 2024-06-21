541905d28af9300f699a5a05e674176a12ab7e7a
Stella Kinja <kinjastella@gmail.com>
2024-06-21 16:00:32 +0300

    Working with promises

--- Documents/Moringa/week5/sync-async.js
+++ Documents/Moringa/week5/sync-async.js
@@ -27,4 +27,93 @@ function orderOnline(){
         console.log('4. pack into shopping bags while waiting for the confirmation');
         
     }
-    superMarketCheckout();
+   // superMarketCheckout();
+
+    /**
+     * PROMISES
+     * A promise is something that will happen in the future, either sucessfully or unsuccessfully.
+     * It has 3 states
+     * Pending - Initiated but not completed 
+     * fulfilled - successful
+     * rejected - failure
+     */
+
+    // example 1
+    function fetchdatafromexternalsource(){
+        console.log('initiated');
+        return new Promise((resolve,reject) => {
+            setTimeout(() => {
+                const fetcheddata = 'This is the data from the external source';
+
+                //resolve = successful
+                //resolve(fetcheddata);
+
+                //reject= failure
+                reject(new Error ('There was a problem fectching the data'));
+
+            }, 2000);
+
+        })
+    }
+
+/*fetchdatafromexternalsource()
+  .then((value) => {
+    console.log('value', value);
+  })
+  .catch((error) => {
+    console.log(error);
+  });*/
+  
+  /*
+  *CHAINING PROMISES
+  *Example - logging in to a chat system and fetching messages
+  promise 1- Logging in
+  promise 2 - fetching messages
+  */
+
+  function logIn(){
+    console.log('initiated log in');
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const fetcheddata = {id: 1, firstName: 'Jane', lastName: 'Doe'};
+
+            //resolve = successful
+            resolve(fetcheddata);
+
+            //reject= failure
+            reject(new Error ('There was a problem logging in the user'));
+
+        }, 2000);
+
+    })
+}
+
+function fetchmessage(userId){
+    console.log('initiated message fetch ...');
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const fetcheddata = ['message 1' , 'message 2' , 'message 3'];
+
+            //resolve = successful
+            //resolve(fetcheddata);
+
+            //reject= failure
+            reject(new Error ('There was a problem fetching messages for the user'));
+
+        }, 2000);
+
+    })
+}
+
+logIn()
+ .then((user) => {
+    console.log('Logged in user is', user);
+    const id = user.id;
+    return fetchmessage(id);
+ })
+ .then((messages) => {
+    console.log('user messages are', messages);
+ })
+ .catch((error) => {
+    console.error(error);
+ });
